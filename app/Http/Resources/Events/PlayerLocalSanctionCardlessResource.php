09af60a52f727cbb4cee7ea73152ae27586cefc6
RodriGarcia12 <rgarciaa125@gmail.com>
2022-11-17 15:23:54 -0300

    Feat: crear API resource para modelo PlayerLocalSanctionCardless.

---
+++ app/Http/Resources/Events/PlayerLocalSanctionCardlessResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Events;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Events\EventResource;
+use App\Http\Resources\Sanctions\SanctionCardlessResource;
+
+class PlayerLocalSanctionCardlessResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'event' => new EventResource($this->event),
+            'sanctionCardless' => new SanctionCardlessResource($this->sanction_cardless),
+            'minute' => $this->minute,
+        ];
+    }
+}
