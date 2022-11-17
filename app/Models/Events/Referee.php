4224174dba5aef8b0df399e526efe2c55b5931b6
RodriGarcia12 <rgarciaa125@gmail.com>
2022-11-17 00:20:50 -0300

    Feat: crear modelos relacionados a eventos.

---
+++ app/Models/Events/Referee.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Events;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Whereabouts\Country;
+
+class Referee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'birth_date',
+        'country_id',
+        'picture'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function events()
+    {
+        return $this->belongsToMany(Event::class)->withTimestamps();
+    }
+}
