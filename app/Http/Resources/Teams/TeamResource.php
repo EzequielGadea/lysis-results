159ca3d6362471230b5d11a02d2755a178953ca5
RodriGarcia12 <rgarciaa125@gmail.com>
2022-11-18 00:47:54 -0300

    Feat: implementar eager loading.

--- app/Http/Resources/Teams/TeamResource.php
+++ app/Http/Resources/Teams/TeamResource.php
@@ -22,9 +22,9 @@ class TeamResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'picture' => $this->picture,
-            'country' => new CountryResource($this->country),
-            'league' => new LeagueResource($this->league),
-            'manager' => new ManagerResource($this->manager),
+            'country' => new CountryResource($this->whenLoaded('country')),
+            'league' => new LeagueResource($this->whenLoaded('league')),
+            'manager' => new ManagerResource($this->whenLoaded('manager')),
             'players' => PlayerTeamResource::collection($this->whenLoaded('playerTeams')),
         ];
     }
