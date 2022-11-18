0edf19282f4f1a403cc8975bf59dc3148fc9b13c
RodriGarcia12 <rgarciaa125@gmail.com>
2022-11-18 06:36:09 -0300

    Feat: agregar rutas

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\TeamController;
-use App\Http\Resources\Players\PlayerTeamResource;
-use App\Models\Players\PlayerTeam;
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\AsideLeftController;
+use App\Http\Controllers\EventByPointController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Resources\Results\MarkNameResource;
-use App\Models\Results\MarkName;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +28,16 @@ Route::controller(TeamController::class)->prefix('team')->group(function () {
     Route::get('{team}', 'show');
 });
 
-Route::get('/test', function() {
-    return MarkNameResource::collection(MarkName::all());
+Route::controller(PlayerController::class)->prefix('player')->group(function () {
+    Route::get('{player}', 'show');
+});
+
+Route::controller(AsideLeftController::class)->prefix('asideLeft')->group(function () {
+    Route::get('getSports', 'getSports');
+    Route::get('getLeagues', 'getLeagues');
+    Route::get('getCountries', 'getCountries');
+});
+
+Route::controller(EventByPointController::class)->prefix('byPoint')->group(function () {
+    Route::get('index', 'index');
 });
